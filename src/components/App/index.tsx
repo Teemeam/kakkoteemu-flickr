1ba30a64591a109f840e5840bdea9a7c383533d5
Teemu Kakko <sormiteippi@windowslive.com>
2022-08-06 22:52:40 +0300

    Try to add photo info

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,17 +4,46 @@ import axios from 'axios';
 /* Import dynamically */
 const Gallery = lazy(() => import('../Gallery'));
 
+declare module namespace {
+  interface Data {
+    photoset: Photoset;
+  }
+  interface Photoset {
+    id: string;
+    photo: Photo[];
+  }
+  interface Photo {
+    id: string;
+    secret: string;
+    server: string;
+  }
+}
+
 const App = () => {
   /* State variables */
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<object[]>([]);
+
+  /* */
+  const getInfo = (photosetData: namespace.Data) => {
+    const photos = photosetData.photoset.photo;
+    for (let i = 0; i < 5; i += 1) {
+      const infoUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=${ import.meta.env.VITE_APP_KEY }&photo_id=${ photos[i].id }&format=json&nojsoncallback=1`;
+      const serveRequest = axios.get(infoUrl)
+        .then((result) => {
+          setData(oldArray => [...oldArray, result.data.photo])
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    }
+  };
 
   /* Get data */
   const getData = () => {
     const photosetUrl = `https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${ import.meta.env.VITE_APP_KEY }&photoset_id=${ import.meta.env.VITE_PHOTOSET_ID }&user_id=${ import.meta.env.VITE_USER_ID }&format=json&nojsoncallback=1`;
     const serveRequest = axios.get(photosetUrl)
       .then((result) => {
-        console.log(result.data);
-        setData(result.data);
+        getInfo(result.data);
       })
       .catch((error) => {
         console.log(error);
--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,41 +1,134 @@
 import { SyntheticEvent, useState } from 'react';
+import axios from 'axios';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 declare module namespace {
-  interface GalleryProps {
-    data: Data;
+  export interface Data {
+    data: RootObject;
   }
-  interface Data {
-    photoset: Photoset;
-    stat: string;
+  export interface RootObject {
+    photo: Photo;
   }
-  interface Photoset {
-    id: string;
-    primary: string;
-    owner: string;
-    ownername: string;
-    photo: Photo[];
-    page: number;
-    per_page: number;
-    perpage: number;
-    pages: number;
-    title: string;
-    total: number;
-  }
-  interface Photo {
+  export interface Photo {
     id: string;
     secret: string;
     server: string;
     farm: number;
-    title: string;
-    isprimary: string;
-    ispublic: number;
-    isfriend: number;
-    isfamily: number;
+    dateuploaded: string;
+    isfavorite: number;
+    license: string;
+    safety_level: string;
+    rotation: number;
+    originalsecret: string;
+    originalformat: string;
+    owner: Owner;
+    title: Title;
+    description: Description;
+    visibility: Visibility;
+    dates: Dates;
+    views: string;
+    editability: Editability;
+    publiceditability: Publiceditability;
+    usage: Usage;
+    comments: Comments;
+    notes: Notes;
+    people: People;
+    tags: Tags;
+    urls: Urls;
+    media: string;
+  }
+  export interface Gift {
+      gift_eligible: boolean;
+      eligible_durations: string[];
+      new_flow: boolean;
+  }
+
+  export interface Owner {
+      nsid: string;
+      username: string;
+      realname: string;
+      location: string;
+      iconserver: string;
+      iconfarm: number;
+      path_alias: string;
+      gift: Gift;
+  }
+
+  export interface Title {
+      _content: string;
+  }
+
+  export interface Description {
+      _content: string;
+  }
+
+  export interface Visibility {
+      ispublic: number;
+      isfriend: number;
+      isfamily: number;
+  }
+
+  export interface Dates {
+      posted: string;
+      taken: string;
+      takengranularity: number;
+      takenunknown: string;
+      lastupdate: string;
+  }
+
+  export interface Editability {
+      cancomment: number;
+      canaddmeta: number;
+  }
+
+  export interface Publiceditability {
+      cancomment: number;
+      canaddmeta: number;
+  }
+
+  export interface Usage {
+      candownload: number;
+      canblog: number;
+      canprint: number;
+      canshare: number;
+  }
+
+  export interface Comments {
+      _content: string;
+  }
+
+  export interface Notes {
+      note: any[];
+  }
+
+  export interface People {
+      haspeople: number;
+  }
+
+  export interface Tag {
+      id: string;
+      author: string;
+      authorname: string;
+      raw: string;
+      _content: string;
+      machine_tag: boolean;
+  }
+
+  export interface Tags {
+      tag: Tag[];
+  }
+
+  export interface Url {
+      type: string;
+      _content: string;
+  }
+
+  export interface Urls {
+      url: Url[];
   }
 }
 
-const Gallery = ({ data }: namespace.GalleryProps) => {
+const Gallery = ({ data }: namespace.Data) => {
   const [renderBuffer, setRenderBuffer] = useState(10);
 
   /* Handle error */
@@ -43,20 +136,24 @@ const Gallery = ({ data }: namespace.GalleryProps) => {
     console.log('Error!');
   };
 
-  const photos = data.photoset.photo;
-  const renderedPhotos = photos.map((photo, i) => {
-    const photoUrl = `https://live.staticflickr.com/${ photo.server }/${ photo.id }_${ photo.secret }_b.jpg`;
-    return (
-      <span key={ `photo_${ i }` }>
-        <LazyLoadImage className='image' src={ photoUrl } alt='' onError={ (e) => handleError(e) }/>
-      </span>
-    );
-  });
+  /* const photos = data.photoset.photo;
+  const reversed = photos.reverse();
+  const renderedPhotos = reversed.map((photo, i) => {
+    if (i <= renderBuffer) {
+      return (
+        <span key={ `photo_${ i }` }>
+          <LazyLoadImage className='image' src={ photoUrl } alt='' onError={ (e) => handleError(e) }/>
+        </span>
+      );
+    }
+  }); */
+
+  console.log(data);
 
   return (
     <div className='outer-image-wrapper'>
       <div className='inner-gallery-wrapper'>
-        { renderedPhotos }
+        Hei
       </div>
     </div>
   )
