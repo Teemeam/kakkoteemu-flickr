342911e984123d30b22c6b92246ce80d297927a9
Teemu Kakko <sormiteippi@windowslive.com>
2022-10-19 19:36:17 +0300

    Make photo descriptions invisible

--- src/components/GalleryImage/index.tsx
+++ src/components/GalleryImage/index.tsx
@@ -1,11 +1,23 @@
-import { SyntheticEvent, useState } from 'react';
+import { SyntheticEvent, useEffect, useState } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const GalleryImage = ({ photo, i }: InfoWrapper) => {
   const [showDescription, setShowDescription] = useState(false);
+  const [descOpacity, setDescOpacity] = useState(false);
+  const [descVisibility, setDescVisibility] = useState(false);
 
-  console.log(photo);
+  /* Transition */
+  useEffect(() => {
+    if (showDescription) {
+      setDescVisibility(true);
+      setDescOpacity(true);
+    } else {
+      setDescOpacity(false);
+      const timer = setTimeout(() => setDescVisibility(false), 150);
+      return () => clearTimeout(timer);
+    }
+  }, [showDescription]);
 
   /* Handle error */
   const handleError = (e: SyntheticEvent<HTMLImageElement, Event>) => {
@@ -15,7 +27,7 @@ const GalleryImage = ({ photo, i }: InfoWrapper) => {
   return (
     <div className='relative sm:h-full h-auto sm:w-auto w-full sm:first-of-type:ml-10 sm:mr-5 mr-0 sm:mb-0 mb-3 cursor-pointer' key={ `photo_${ i }` } onClick={ () => setShowDescription(!showDescription) } onMouseLeave={ () => setShowDescription(false) }>
       <LazyLoadImage className='max-w-none sm:h-full h-auto sm:w-auto w-full' src={ `https://live.staticflickr.com/${ photo?.server }/${ photo?.id }_${ photo?.originalsecret }_h.jpg` } alt='' effect='blur' height='100%' onError={ (e) => handleError(e) }/>
-      <div className={ 'absolute top-0 right-0 bottom-0 left-0 transition-opacity bg-black/[.8] ' + (showDescription ? 'opacity-100' : 'opacity-0') }>
+      <div className={ 'absolute top-0 right-0 bottom-0 left-0 transition-opacity bg-black/[.8] ' + (descOpacity ? 'opacity-100' : 'opacity-0') + (descVisibility ? ' visible' : ' invisible') }>
         <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12'>
           <p className='font-montserrat font-medium text-sm text-center pb-5 text-slate-50'>{ photo?.description?._content }</p>
           <p className='font-montserrat font-medium text-xs text-center text-slate-50'>
