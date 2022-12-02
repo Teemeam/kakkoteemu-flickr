c4a44216c1bc3b503ffdb7381ed179687a0aad05
Teemu Kakko <sormiteippi@windowslive.com>
2022-12-02 22:47:16 +0200

    Style load more button

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -3,6 +3,9 @@ import { useState } from 'react';
 /* Components */
 import GalleryImage from '../GalleryImage';
 
+/* Assets */
+import icon from '../../assets/android-chrome-192x192.png';
+
 const Gallery = ( { height, data }: CompleteData) => {
   const [renderBuffer, setRenderBuffer] = useState<number>(5);
 
@@ -19,8 +22,10 @@ const Gallery = ( { height, data }: CompleteData) => {
     <div className='sm:h-[85vh] h-auto sm:w-auto w-11/12 mx-auto scroll-smooth overflow-x-scroll overflow-y-hidden scrollbar-hide'>
       <div className='sm:h-full h-auto sm:w-auto w-full flex sm:flex-row flex-col'>
         { renderedPhotos }
-        <div className='relative sm:pr-10'>
-          <button className='sm:min-h-auto min-h-100px sm:h-full h-auto sm:min-w-200px sm:w-auto w-full font-montserrat font-light text-sm text-center bg-neutral-200 hover:bg-neutral-300 transition-colors text-black' onClick={ () => setRenderBuffer(renderBuffer + 5) }>Load more</button>
+        <div className='flex justify-center items-center sm:pr-10'>
+          <button type='button' className='relative h-20 w-20 font-montserrat font-light text-sm text-center opacity-80 hover:opacity-100 hover:rotate-45 transition-opacity transition-transform text-black' aria-label='Load more' onClick={ () => setRenderBuffer(renderBuffer + 5) }>
+            <img className='block w-full' src={ icon } alt=''/>
+          </button>
         </div>
       </div>
     </div>
